6c3729a2de15352a8c4fc57bce381336a28ac81a
James King <j@lostwith.me>
2013-11-25 19:21:55 +0000

    Broke out json stuff and css attempts

---
+++ app/scripts/db.js
@@ -0,0 +1,42 @@
+
+
+var jobject = {
+	'entries': [
+		{ 'hex':'accede', 'definition':'agree to a demand, request, or treaty'},
+		{ 'hex':'bacaba', 'definition':' a palm of the genus Oenocarpus the drupelike fruits of which yield oil used in soap manufacture'},
+		{ 'hex':'baccae', 'definition':'plural; an indehiscent fruit derived from a single ovary having one or many seeds within a fleshy wall or pericarp'},
+		{ 'hex':'beaded', 'definition':'decorate or cover with beads'},
+		{ 'hex':'bedded', 'definition':'having a specified number or type of beds'},
+		{ 'hex':'bedead', 'definition':''},
+		{ 'hex':'bedeaf', 'definition':''},
+		{ 'hex':'decade', 'definition':'a period of ten years'},
+		{ 'hex':'deface', 'definition':'spoil the surface or appearance of (something), for example by drawing or writing on it'},
+		{ 'hex':'efface', 'definition':'erase (a mark) from a surface'},
+		{ 'hex':'facade', 'definition':'the principal front of a building, that faces on to a street or open space'},
+
+		{ 'hex':'aba', 'definition':'' },
+		{ 'hex':'abb', 'definition':'' },
+		{ 'hex':'ace', 'definition':'' },
+		{ 'hex':'add', 'definition':'' },
+		{ 'hex':'ade', 'definition':'' },
+		{ 'hex':'baa', 'definition':'' },
+		{ 'hex':'bac', 'definition':'' },
+		{ 'hex':'bad', 'definition':'' },
+		{ 'hex':'bae', 'definition':'' },
+		{ 'hex':'bed', 'definition':'' },
+		{ 'hex':'bee', 'definition':'' },
+		{ 'hex':'cab', 'definition':'' },
+		{ 'hex':'cad', 'definition':'' },
+		{ 'hex':'cee', 'definition':'' },
+		{ 'hex':'dab', 'definition':'' },
+		{ 'hex':'dad', 'definition':'' },
+		{ 'hex':'dae', 'definition':'' },
+		{ 'hex':'deb', 'definition':'' },
+		{ 'hex':'dee', 'definition':'' },
+		{ 'hex':'ebb', 'definition':'' },
+		{ 'hex':'fad', 'definition':'' },
+		{ 'hex':'fae', 'definition':'' },
+		{ 'hex':'fed', 'definition':'' },
+		{ 'hex':'fee', 'definition':'' }
+	]
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,21 +3,7 @@
 $(function() {
 	'use strict';
 
-	var jobject = {
-		'entries': [
-			{ 'hex':'accede', 'definition':'agree to a demand, request, or treaty'},
-			{ 'hex':'bacaba', 'definition':''},
-			{ 'hex':'baccae', 'definition':''},
-			{ 'hex':'beaded', 'definition':'decorate or cover with beads'},
-			{ 'hex':'bedded', 'definition':'having a specified number or type of beds'},
-			{ 'hex':'bedead', 'definition':''},
-			{ 'hex':'bedeaf', 'definition':''},
-			{ 'hex':'decade', 'definition':'a period of ten years'},
-			{ 'hex':'deface', 'definition':'spoil the surface or appearance of (something), for example by drawing or writing on it'},
-			{ 'hex':'efface', 'definition':'erase (a mark) from a surface'},
-			{ 'hex':'facade', 'definition':'the principal front of a building, that faces on to a street or open space'}
-		]
-	};
+
 
 	// var cheese = Handlebars.colour(jobject.entries[2]);
 
@@ -26,5 +12,11 @@ $(function() {
 		$('body').append(Handlebars.entry(jobject.entries[i]));
 	}
 
+	var causeRepaintsOn = $("h1, h2, h3, p");
+
+	$(window).resize(function() {
+	  causeRepaintsOn.css("z-index", 1);
+	  console.log('repaint')
+	});
 	// $('body').append(cheese);
 });
