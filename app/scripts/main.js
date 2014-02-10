c742e1a39a7ba54f7d26c48fdd85b1ddd927ec2f
James King <james@crystalpresentations.com>
2014-02-10 13:54:17 +0000

    junk cleanout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,58 +1,55 @@
 /* global Handlebars, jobject */
 
-var myObject = {
+var hexList = {
 
-    myObject: myObject,
-
-
+    hexList: hexList,
 
     init: function () {
-
-        // console.log('hello');
-
 
         var causeRepaintsOn = $("h1, h2, h3, p");
 
         $(window).resize(function() {
 
-            myObject.repaint();
+            hexList.repaint();
 
         });
 
-        myObject.build();
+        hexList.build();
 
     },
 
     build: function () {
 
         for (var i = 0; i < jobject.entries.length; i++) {
-            // var cheese = myObject.hexToRgb(jobject.entries[i].hex);
-            // console.log(cheese);
 
-            var values = {entry: jobject.entries[i], nextEntry: jobject.entries[i+1], iter: 3*(10 - i), iter2: -3*(10 - i)};
+            var values = {entry: jobject.entries[i], nextEntry: jobject.entries[i+1]};
+
             $('body').append(Handlebars.entry(values));
 
         }
 
         $( window ).on('scroll', function (event) {
 
-            myObject.handleCurrent();
+            hexList.handleCurrent();
 
         });
 
-        myObject.handleCurrent();
+        hexList.handleCurrent();
 
     },
 
     handleCurrent: function () {
+        var thing, location, height;
 
         $('.entry').each( function () {
 
-            var thing = $( this );
+            thing = $( this );
 
-            var location = thing.position().top - $( window ).scrollTop();
+            location = thing.position().top - $( window ).scrollTop();
 
-            if ( location < ($( window ).height() / 2) && location > -($( window ).height() / 2) ) {
+            height = $( window ).height() / 2;
+
+            if ( location < height && location > -height ) {
 
                 thing.children('.absolute').css('opacity','1');
 
@@ -65,20 +62,6 @@ var myObject = {
         });
     },
 
-    hexToRgb: function (hex) {
-        var bigint = parseInt(hex, 16);
-        var r = (bigint >> 16) & 255;
-        var g = (bigint >> 8) & 255;
-        var b = bigint & 255;
-
-        return r + ',' + g + ',' + b;
-    },
-
-    getMidPoint: function (hex1, hex2) {
-
-
-    },
-
     repaint: function () {
         causeRepaintsOn.css('z-index', 1);
     }
@@ -87,6 +70,6 @@ var myObject = {
 $(function() {
 	'use strict';
 
-    myObject.init();
+    hexList.init();
 
 });
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -3,73 +3,19 @@ this["Handlebars"] = this["Handlebars"] || {};
 this["Handlebars"]["entry"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, stack2, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
   buffer += "<div class=\"entry\" style=\"background: #"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + ";\" id=>\n<!-- <div class=\"entry\" style=\"background: #"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "; z-index: ";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "\"> -->\n    <!-- <div class=\"before\" style=\"background: linear-gradient(transparent, #F5E2D6);\"></div> -->\n    <!-- <div class=\"after\" style=\"background: linear-gradient(#F5E2D6, transparent);\"></div> -->\n    <div class=\"absolute\">\n    <!-- <div class=\"absolute\" style=\"background:#"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "; -webkit-transform: translate(";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px,";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px);\"> -->\n    <!-- <div class=\"absolute\" style=\"background:#"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "; z-index: ";
-  if (stack2 = helpers.iter2) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter2); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "; -webkit-transform: translate(";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px,";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px);\"> -->\n    <!-- <div class=\"absolute\" style=\"background:#"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "; z-index: 5; -webkit-transform: translate(";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px,";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px);\"> -->\n    <!-- <div class=\"absolute\" style=\"background:#"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "; -webkit-transform: translate(";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px,";
-  if (stack2 = helpers.iter) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
-  else { stack2 = (depth0 && depth0.iter); stack2 = typeof stack2 === functionType ? stack2.call(depth0, {hash:{},data:data}) : stack2; }
-  buffer += escapeExpression(stack2)
-    + "px);\"> -->\n        <h1>#"
+    + ";\">\n\n    <div class=\"absolute\">\n\n        <h1>#"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</h1>\n        <p>"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.definition)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</p>\n    </div>\n    <div class=\"gradient\" style=\"background: linear-gradient(#"
+    + "</p>\n\n    </div>\n    <div class=\"gradient\" style=\"background: linear-gradient(#"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + ", #"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.nextEntry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + ");\"></div>\n    <!-- <div class=\"gradient\" style=\"background: linear-gradient(#"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.nextEntry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + ", #"
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.entry)),stack1 == null || stack1 === false ? stack1 : stack1.hex)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + ");\"></div> -->\n</div>\n";
+    + ");\"></div>\n</div>\n";
   return buffer;
   });
