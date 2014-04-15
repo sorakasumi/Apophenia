43883ce977ea46e5854c044930fbf80425479c2a
James King <james@crystalpresentations.com>
2014-04-15 17:38:22 +0100

    fullscreen sizeable tessellating hexagons

--- app/scripts/paperscript.js
+++ app/scripts/paperscript.js
@@ -1,4 +1,4 @@
-/* global Path, Point */
+/* global Path, Point, view */
 
 $(function() {
     'use strict';
@@ -21,9 +21,13 @@ $(function() {
 
         draw: function () {
 
-            var hex = new Path.RegularPolygon(new Point(this.x, this.y), this.sides, this.radius);
+            var hex = new Path.RegularPolygon( new Point(this.x, this.y), this.sides, this.radius );
 
-            hex.strokeColor = 'black';
+            hex.strokeColor = 'lightgrey';
+
+            // if (this.rotate) { hex.rotate(10); }
+            // hex.rotate(this.rotate);
+            // hex.fillColor = 'lightgrey';
 
         },
 
@@ -31,26 +35,71 @@ $(function() {
 
     var hexPaper = {
 
+        radius: 10,
+
+        sides: 6,
+
+        rotate: 10,
+
         init: function () {
-            var i, i2, hex;
+            var i, i2, hex, alternateX = 0, alternateY = 0, rows, cols, width, height;
 
-            for(i = 0; i < 10; i++) {
+            width = this.radius * 2;
+            height = this.radius * 2;
 
-                for(i2 = 0; i2 < 10; i2++) {
+            // alternateX = Math.tan(this.rotate) * width;
 
-                    hex = $.extend({}, hexagon, {x:(i2+1)*100,y:(i+1)*110});
+            // if (this.rotate) {
+            //     width *= 0.9;
+            // } else {
+            height *= 0.9;
+            // }
+
+            cols = view.size.width / width + 1;
+            rows = view.size.height / height + 1;
+
+            for (i = 0; i < rows; i++) {
+
+                // if (!this.rotate) {
+                alternateX = (i%2 === 0) ? 0 : this.radius;
+                // }
+
+                // alternateX += 20;
+
+                for (i2 = 0; i2 < cols; i2++) {
+
+                    // alternateY += 20;
+
+                    // if (this.rotate) {
+                    //     alternateY = (i2%2 === 0) ? 0 : this.radius;
+                    // }
+
+                    hex = $.extend( {}, hexagon, {
+                        radius: this.radius,
+                        sides: this.sides,
+                        rotate: this.rotate,
+                        x: i2 * width - alternateX,
+                        y: i * height - alternateY
+                    } );
 
                     hex.init();
 
                 }
+
+                // alternateY = 0;
             }
 
-            console.log(hexagon.x);
+            console.log(view.size.width);
+
         },
 
     };
 
 
     hexPaper.init();
+
+    // function isEven(value) {
+    //     return (value%2 == 0);
+    // }
 
 });
