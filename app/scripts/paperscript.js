ffbda88b07ae9983616f9417d0ba2e283e911404
James King <james@crystalpresentations.com>
2014-04-16 13:02:00 +0100

    Pointify everything, and setup test bounds

--- app/scripts/paperscript.js
+++ app/scripts/paperscript.js
@@ -1,4 +1,4 @@
-/* global Path, Point, view */
+/* global Path, Point, Size, view, Group */
 
 $(function() {
     'use strict';
@@ -21,7 +21,7 @@ $(function() {
 
         draw: function () {
 
-            var hex = new Path.RegularPolygon( new Point(this.x, this.y), this.sides, this.radius );
+            var hex = new Path.RegularPolygon( this.point, this.sides, this.radius );
 
             hex.strokeColor = 'lightgrey';
 
@@ -39,47 +39,120 @@ $(function() {
 
         sides: 6,
 
-        rotate: 10,
+        rotate: 30,
 
         init: function () {
-            var i, i2, hex, alternateX = 0, alternateY = 0, rows, cols, width, height;
 
-            width = this.radius * 2;
-            height = this.radius * 2;
+            // test junk --------------------------
+
+            var bounds = new Path.Rectangle(view.center, new Size(500, 300));
+            bounds.position = view.center;
+            bounds.strokeColor = 'red';
+
+            var rotateBounds = new Path.Circle(view.center, (pythagAB(500,300) / 2));
+            rotateBounds.strokeColor = 'blue';
+
+            var squareRotateBounds = new Path.Rectangle(view.center, new Size(pythagAB(500,300), pythagAB(500,300)));
+            squareRotateBounds.position = view.center;
+            squareRotateBounds.strokeColor = 'green';
+
+            // end junk --------------------------
+
+            var i, i2, hex, alternate = 0, rows, cols, width, height, rads;
+            // var i, i2, hex, alternateX = 0, alternateY = 0, rows, cols, width, height, rads;
+
+            var hexGroup = new Group();
+            hexGroup.strokeColor = 'black';
+
+            // width = this.radius * 2;
+            // height = this.radius * 2;
+
+            var hexSize = new Size(this.radius * 2);
+            hexSize.height *= 0.9;
+            // console.log(bounds.size);
+            // console.log(hexSize);
+            // console.log(bounds.bounds.size / hexSize);
+
+            var gridSize = new Size(bounds.bounds.size / hexSize + 1);
+            // console.log(gridSize);
+
 
             // alternateX = Math.tan(this.rotate) * width;
 
             // if (this.rotate) {
             //     width *= 0.9;
             // } else {
-            height *= 0.9;
             // }
 
-            cols = view.size.width / width + 1;
-            rows = view.size.height / height + 1;
+            // rads = this.rotate * (Math.PI/180);
 
-            for (i = 0; i < rows; i++) {
+            // var diag = Math.sqrt(Math.pow(view.size.width,2) + Math.pow(view.size.height,2));
+            // var spareHeight = (diag - view.size.height) / 2;
+            // var spareWidth = (diag - view.size.width) / 2;
+
+            // var diag = pythagAB(bounds.height);
+            // cols = bounds.bounds.width / width + 1;
+            // console.log(bounds.bounds);
+            // cols = diag / width + 1;
+            // rows = bounds.bounds.height / height + 1;
+            // rows = diag / height + 2;
+
+
+
+            for (i = 0; i < gridSize.height; i++) {
 
                 // if (!this.rotate) {
-                alternateX = (i%2 === 0) ? 0 : this.radius;
+
+                alternate = (i%2 === 0) ? 0 : this.radius;
+                // alternateX = (i%2 === 0) ? 0 : this.radius;
                 // }
 
                 // alternateX += 20;
 
-                for (i2 = 0; i2 < cols; i2++) {
+                for (i2 = 0; i2 < gridSize.width; i2++) {
+                    // var x, y;
+
+                    // var point = new Point(bounds.bounds.x + i2 * hexSize.width + alternate, bounds.bounds.y + i * hexSize.height);
+                    var point = new Point(bounds.bounds.point + new Point(i2,i) * hexSize);
+                    point.x += alternate;
+                    // var x, y, newX, newY;
 
                     // alternateY += 20;
 
                     // if (this.rotate) {
                     //     alternateY = (i2%2 === 0) ? 0 : this.radius;
                     // }
+                    // x = bounds.bounds.x + i2 * width + alternate;
+                    // x = i2 * width - alternateX - spareWidth;
+                    // y = bounds.bounds.y + i * height;
+                    // y = i * height - alternateY - spareHeight;
+
+                    // var notCenter = new Point(diag/2,diag/2);
+
+                    // var point1 = new Point(x,y);
+                    // var point2 = point1 - notCenter;
+
+                    // x = point2.x;
+                    // y = point2.y;
+
+                    // newX = x * Math.cos(rads) - y * Math.sin(rads);
+                    // newY = x * Math.sin(rads) + y * Math.cos(rads);
+
+                    // var point3 = new Point(newX,newY);
+                    // var point4 = point3 + notCenter;
+
+                    // newX = point4.x;
+                    // newY = point4.y;
 
                     hex = $.extend( {}, hexagon, {
                         radius: this.radius,
                         sides: this.sides,
                         rotate: this.rotate,
-                        x: i2 * width - alternateX,
-                        y: i * height - alternateY
+                        point: point,
+                        // x: newX,
+                        // x: x,
+                        // y: newY
+                        // y: y
                     } );
 
                     hex.init();
@@ -89,7 +162,7 @@ $(function() {
                 // alternateY = 0;
             }
 
-            console.log(view.size.width);
+            // console.log(view.center);
 
         },
 
@@ -101,5 +174,11 @@ $(function() {
     // function isEven(value) {
     //     return (value%2 == 0);
     // }
+
+    function pythagAB (a,b) {
+
+        return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+    }
 
 });
