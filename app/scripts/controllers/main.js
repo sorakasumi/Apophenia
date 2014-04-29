18c6900f63543f4f2243ef308b33678815e82d41
James King <james@crystalpresentations.com>
2014-04-29 16:24:24 +0100

    custom kinds filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,9 +26,47 @@ angular.module('hexControls', [])
 
             }
 
+        })
 
+    .filter('kindfilter',
 
+        function() {
 
+            // filter takes (input, arguments)
 
+            return function(colours, filterKinds) {
+
+                if (filterKinds) {
+                    var kinds, out = [];
+
+                    for (var i = 0; i < colours.length; i++) {
+
+                        // check if 'kind' is an array, if not wrap it in an array
+
+                        kinds = (colours[i].kind instanceof Array) ? colours[i].kind : [colours[i].kind];
+
+                        // loop through till matching kind is found
+
+                        for (var i2 = 0; i2 < kinds.length; i2++) {
+
+                            // check if kind is found in active filters
+
+                            if (filterKinds[kinds[i2]] === true) {
+
+                                out.push(colours[i]);
+
+                                break;
+
+                            }
+
+                        }
+
+                    }
+
+                    return out;
+
+                }
+
+            };
 
         });
